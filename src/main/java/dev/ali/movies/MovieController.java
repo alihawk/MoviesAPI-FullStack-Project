c3c8e18d93e71e161204c0e3ff817c53af64c330
alihawk <m.ali532001@gmail.com>
2024-04-21 23:42:22 +0500

    Initial Commit

---
+++ src/main/java/dev/ali/movies/MovieController.java
@@ -0,0 +1,33 @@
+package dev.ali.movies;
+
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+    @CrossOrigin(origins = "http://localhost:3000") // Specify allowed origins
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+
+
+
+    }
+    @CrossOrigin(origins = "http://localhost:3000") // Specify allowed origins
+    @GetMapping("/imdb/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovieByImdbId(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
+
+    }
+}
---
+++ src/main/java/dev/ali/movies/MovieRepository.java
@@ -0,0 +1,15 @@
+package dev.ali.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
+
+
+
+
+}
